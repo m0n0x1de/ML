eb61236087f2dc69a07998e2045e7510b19253a1
Bogdan Mariniuc <61044547+m0n0x1de@users.noreply.github.com>
2024-05-04 11:32:46 +0300

    added elongation feature

--- Machine Learning - Radu/common/featureFunctions.js
+++ Machine Learning - Radu/common/featureFunctions.js
@@ -1,35 +1,49 @@
-const featureFunctions ={};
-
-featureFunctions.getPathCount=(paths)=>{
-    return paths.length;
+if(typeof geometry === "undefined"){
+   geometry = require("./geometry.js");
 }
 
-featureFunctions.getPointCount=(paths)=>{
-    const points=paths.flat();
-    return points.length;
+const featureFunctions = {};
+
+featureFunctions.getPathCount = (paths) => {
+   return paths.length;
+};
+
+featureFunctions.getPointCount = (paths) => {
+   const points = paths.flat();
+   return points.length;
+};
+
+featureFunctions.getWidth = (paths) => {
+   const points = paths.flat();
+   const x = points.map((p) => p[0]);
+   const min = Math.min(...x);
+   const max = Math.max(...x);
+   return max - min;
+};
+
+featureFunctions.getHeight = (paths) => {
+   const points = paths.flat();
+   const y = points.map((p) => p[1]);
+   const min = Math.min(...y);
+   const max = Math.max(...y);
+   return max - min;
+};
+
+featureFunctions.getElongation = (paths) => {
+   const points = paths.flat();
+   const { width, height } = geometry.minimumBoundingBox({ points});
+   return (Math.max(width, height) +1) / 
+            (Math.min(width, height) +1);
 }
 
-featureFunctions.getWidth=(paths)=>{
-    const points=paths.flat();
-    const x=points.map(p=>p[0]);
-    const min=Math.min(...x);
-    const max=Math.max(...x);
-    return max-min;
-}
-
-featureFunctions.getHeight=(paths)=>{
-    const points=paths.flat();
-    const y=points.map(p=>p[1]);
-    const min=Math.min(...y);
-    const max=Math.max(...y);
-    return max-min;
-}
-
-featureFunctions.inUse=[
-    {name:"Width", function:featureFunctions.getWidth},
-    {name:"Height", function:featureFunctions.getHeight}
+featureFunctions.inUse = [
+   //{name:"Path Count",function:featureFunctions.getPathCount},
+   //{name:"Point Count",function:featureFunctions.getPointCount},
+   { name: "Width", function: featureFunctions.getWidth },
+   { name: "Height", function: featureFunctions.getHeight },
+   { name: "Elongation", function: featureFunctions.getElongation },
 ];
 
-if(typeof module!=='undefined'){
-    module.exports=featureFunctions;
+if (typeof module !== "undefined") {
+   module.exports = featureFunctions;
 }
